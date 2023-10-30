7b602b11ef646199247aee6a31bbe2b5fc73b202
gigih santoso <gigih985042@gmail.com>
2023-10-30 16:04:18 +0700

    update  component nav-drawer dan nav-item

--- src/app/layouts/documentations/documentations.component.ts
+++ src/app/layouts/documentations/documentations.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TopBarModel } from 'src/app/components/app-bars/top-bar/top-bar.enum';
 import { MainModel } from 'src/app/components/main/main.enum';
+import { NavDrawerModel } from 'src/app/components/navigation/nav-drawer/nav-drawer.enum';
+import { NavItemModel } from 'src/app/components/navigation/nav-item/nav-item.enum';
 
 @Component({
   selector: 'gks-documentations',
@@ -8,6 +10,14 @@ import { MainModel } from 'src/app/components/main/main.enum';
   styleUrls: ['./documentations.component.css']
 })
 export class DocumentationsComponent {
+  sideMenu: boolean = false;
+
   largeModelMain = MainModel.large
   modelTopBar: string = TopBarModel.center;
+  modelNavDrawer: string = NavDrawerModel.modal;
+  modelNavDrawerItem: string = NavItemModel.navdrawer;
+
+  menuAction(){
+    this.sideMenu = !this.sideMenu  
+  }
 }
