6ee81b8e08a4758099c4a6278998ea1c1d122039
gigih santoso <gigih985042@gmail.com>
2023-10-30 10:15:46 +0700

    added ViewEncapsulation None

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'gks-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ButtonComponent {
   @Input() type: string | null = null;
