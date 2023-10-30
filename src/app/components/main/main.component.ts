e91d8dbce3db48c4e8d9d88c421e61a4947baf6f
gigih santoso <gigih985042@gmail.com>
2023-10-30 10:16:37 +0700

    added ViewEncapsulation None

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'gks-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainComponent {
   @Input() model: string = '';
