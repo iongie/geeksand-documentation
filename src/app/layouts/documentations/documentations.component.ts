762e0e90214c17c739ca0195e32f5c409d597170
gigih santoso <gigih985042@gmail.com>
2023-10-30 10:46:27 +0700

    add top bar in layout documentation

--- src/app/layouts/documentations/documentations.component.ts
+++ src/app/layouts/documentations/documentations.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TopBarModel } from 'src/app/components/app-bars/top-bar/top-bar.enum';
+import { MainModel } from 'src/app/components/main/main.enum';
 
 @Component({
   selector: 'gks-documentations',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./documentations.component.css']
 })
 export class DocumentationsComponent {
-
+  largeModelMain = MainModel.large
+  modelTopBar: string = TopBarModel.center;
 }
