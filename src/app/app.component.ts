855f8905c4cf244d180800bbbde16ee04757614f
gigih santoso <gigih985042@gmail.com>
2023-10-30 09:06:34 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gks-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'geeksand-documentation';
+}
